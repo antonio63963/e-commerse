2e3f578921efc82c454aeecbed5931cc53e19499
Antonio <shotvideo25@gmail.com>
2023-10-15 20:41:46 +0300

    nav user, routing admin

---
+++ app/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { AdminLayoutComponent } from './admin/shared/admin-layout/admin-layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    MainPageComponent,
+    ProductPageComponent,
+    CartPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
